d7bef8b8190117bd258a5731ffc5668fd35f52f8
dubarikalita <dubarikalita4114@gmail.com>
2026-05-26 18:51:48 +0530

    fix: move boto3 init inside handler to fix pytest import error

--- lambda/counter.py
+++ lambda/counter.py
@@ -4,8 +4,6 @@ from decimal import Decimal
 
 import boto3
 
-dynamodb = boto3.resource("dynamodb")
-
 TABLE_NAME = os.environ.get(
     "TABLE_NAME",
     "serverless-resume-platform-counter",
@@ -13,6 +11,14 @@ TABLE_NAME = os.environ.get(
 
 
 def lambda_handler(event, context):
+    # boto3 initialised inside the handler so pytest can import
+    # this file without needing real AWS credentials or a region.
+    # In Lambda, the region is always available in the environment.
+    dynamodb = boto3.resource(
+        "dynamodb",
+        region_name=os.environ.get("AWS_DEFAULT_REGION", "ap-south-1"),
+    )
+
     table = dynamodb.Table(TABLE_NAME)
 
     response = table.update_item(
